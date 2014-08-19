9605b624f7a6e532af711c865ef049061465a50e
anaer <lvcunna@gmail.com>
2014-08-19 10:11:54 +0800

    commit

--- assets/themes/twitter/js/jquery.scroll.js
+++ assets/themes/twitter/js/jquery.scroll.js
@@ -1,41 +1,20 @@
 //为避免冲突，将我们的方法用一个匿名方法包裹起来
 (function($) {
 
-  function getScroll(){
-           var bodyTop = 0;
-           if (typeof window.pageYOffset != 'undefined') {
-                   bodyTop = window.pageYOffset;
-           } else if (typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') {
-                   bodyTop = document.documentElement.scrollTop;
-           }
-           else if (typeof document.body != 'undefined') {
-                   bodyTop = document.body.scrollTop;
-           }
-           return bodyTop
-  }
-  //插件名字
-  $.fn.scrollShow=function(options) {
+    //扩展这个方法到jquery
+    $.fn.extend({
 
-      //设置默认值并用逗号隔开
-      var defaults = {
-          topdiv: '50px';
-      }
+        //插件名字
+        scrollShow: function() {
 
-      var o =  $.extend(defaults, options);
-      alert(o.topdiv);
-      //遍历匹配元素的集合
-          //在这里编写相应代码进行处理
-        $(window).scroll(function(){
-            alert('触发了');
-            clearInterval(timer);
-            var topScroll=getScroll();
-            var topDiv=o.topdiv;
-            var top=topScroll+parseInt(topDiv);
-            timer=setInterval(function(){
-                     $(this).animate({"top":top},500);
-            },500)
-        })
-        return this;
-  }
+            //遍历匹配元素的集合
+            return this.each(function() {
+              alert('插件进来了');
+                //在这里编写相应代码进行处理
+
+            });
+        }
+    });
+
  //传递jQuery到方法中，这样我们可以使用任何javascript中的变量来代替"$"
 })(jQuery);
