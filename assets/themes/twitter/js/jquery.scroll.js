9d1195339c5a5b3083e93e167d511770abcbeff5
anaer <lvcunna@gmail.com>
2014-08-19 09:38:50 +0800

    commit

--- assets/themes/twitter/js/jquery.scroll.js
+++ assets/themes/twitter/js/jquery.scroll.js
@@ -1,6 +1,19 @@
 //为避免冲突，将我们的方法用一个匿名方法包裹起来
 (function($) {
 
+    function getScroll(){
+             var bodyTop = 0;
+             if (typeof window.pageYOffset != 'undefined') {
+                     bodyTop = window.pageYOffset;
+             } else if (typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') {
+                     bodyTop = document.documentElement.scrollTop;
+             }
+             else if (typeof document.body != 'undefined') {
+                     bodyTop = document.body.scrollTop;
+             }
+             return bodyTop
+    }
+
     //扩展这个方法到jquery
     $.fn.extend({
 
@@ -9,9 +22,7 @@
 
             //设置默认值并用逗号隔开
             var defaults = {
-                padding: 20,
-                mouseOverColor : '#000000',
-                mouseOutColor : '#ffffff'
+                topdiv: 50px;
             }
 
             var options =  $.extend(defaults, options);
@@ -19,9 +30,17 @@
             //遍历匹配元素的集合
             return this.each(function() {
                 //在这里编写相应代码进行处理
-                var o = options;
-                alert(o.padding);
-
+              var o = options;
+              var obj = $(this);
+              $(window).scroll(function(){
+                  clearInterval(timer);
+                  var topScroll=getScroll();
+                  var topDiv=o.topdiv;
+                  var top=topScroll+parseInt(topDiv);
+                  timer=setInterval(function(){
+                           $(this).animate({"top":top},500);
+                  },500)
+              })
             });
         }
     });
