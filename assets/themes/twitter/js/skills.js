352dcf0b28877657831f26e96c901ca79dcb4037
anaer <lvcunna@gmail.com>
2014-09-08 00:20:15 +0800

    commit

--- assets/themes/twitter/js/skills.js
+++ assets/themes/twitter/js/skills.js
@@ -448,5 +448,6 @@ code[221] = "apt-cache show k3b";
 
 
 index = Math.floor(Math.random() * tips.length);
-document.write("&nbsp;"+tips[index]+':&nbsp;&nbsp;<code>'+code[index]+'</code>');
+document.getElementById("skilltip").innerHTML="&nbsp;"+tips[index]+':&nbsp;&nbsp;<code>'+code[index]+'</code>';
+//document.write("&nbsp;"+tips[index]+':&nbsp;&nbsp;<code>'+code[index]+'</code>');
 
