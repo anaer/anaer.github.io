ac897f180114db1d95320fdbba67c6e6bc54910c
a._-0 <a._-0@139.com>
2023-02-24 10:03:38 +0800

    单引号需要使用标签转义符

--- source/src/card/common/linktab.js
+++ source/src/card/common/linktab.js
@@ -21,7 +21,7 @@ module.exports = function(option) {
             '                <li style="padding:8px">'+
             '                  <span >'+
             '                    <a data-on="?m=<%=config.isEnv ? "goEnv" : "go"%>&target=_blank" data-url="<%=list[i].href%>" title="<%=list[i].title%>">'+
-            '                      <img style="height:16px; width:16px" src="<%=list[i].favicon%>" onerror="this.src='favicon/favicon.ico'">  </img><%=list[i].name%>'+
+            '                      <img style="height:16px; width:16px" src="<%=list[i].favicon%>" onerror="this.src=&apos;favicon/favicon.ico&apos;">  </img><%=list[i].name%>'+
             '                    </a>'+
             '                  </span>' +
             '                <button style="display:none" type="button" class="am-btn am-btn-default am-btn-xs" style="float:right" data-on="?m=goQR<%=config.isEnv ? "&env=1" : ""%>" data-url="<%=list[i].href%>">QR</button>'+
@@ -30,7 +30,7 @@ module.exports = function(option) {
             '                <li style="padding:8px" class="expand am-hide" >'+
             '                  <span >'+
             '                    <a data-on="?m=<%=config.isEnv ? "goEnv" : "go"%>&target=_blank" data-url="<%=list[i].href%>" title="<%=list[i].title%>">'+
-            '                      <img style="height:16px; width:16px" src="<%=list[i].favicon%>" onerror="this.src='favicon/favicon.ico'">  </img><%=list[i].name%>'+
+            '                      <img style="height:16px; width:16px" src="<%=list[i].favicon%>" onerror="this.src=&apos;favicon/favicon.ico&apos;">  </img><%=list[i].name%>'+
             '                    </a>'+
             '                  </span>' +
             '                <button style="display:none" type="button" class="am-btn am-btn-default am-btn-xs" style="float:right" data-on="?m=goQR<%=config.isEnv ? "&env=1" : ""%>" data-url="<%=list[i].href%>">QR</button>'+
